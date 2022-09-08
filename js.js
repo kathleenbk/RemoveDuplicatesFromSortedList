4151c1200fd0e9a98831f17c6d1c692c298aed9f
kathleenbk <katie.kava3@gmail.com>
2022-09-08 10:57:50 -0800

    init commit

---
+++ js.js
@@ -0,0 +1,40 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+// Example 2:
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+// Constraints:
+// The number of nodes in the list is in the range [0, 300].
+// -100 <= Node.val <= 100
+// The list is guaranteed to be sorted in ascending order.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head) return head; //edge case
+
+    let current = head;
+
+    while (current && current.next) {
+        if (current.next.val === current.val) { //if the current value is equal to the next value 
+            current.next = current.next.next //change the duplicate to the next value in the list
+        } else {current = current.next} //move to the next value
+        
+    };
+
+    return head;
+};
